1a0897137f6a5210b65ffce9c2471dd12c554197
Eradid <corentin.montiege@gmail.com>
2022-04-24 15:28:10 +0200

    Exercice 3 test

--- todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
@@ -9,5 +9,22 @@ class TodolistApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
+	
+	@Test
+    void justCreatedTodoItem() {
+    	todoItem = new TodoItem("1", Instant.now(),"todoiItem 1 créé");
+    	assertFalse(todoiItem.finalContent().contains("[LATE!] "));
+    }
+    
+    @Test
+    void Created48hAgoTodoItem() {
+    	todoItem = new TodoItem("2",Instant.now().minus(48,ChronoUnit.HOURS),"todoItem 2 créé il y a 48H");
+    	assertTrue(todoItem.finalContent().contains("[LATE!] ");
+    }
+    
+    @Test
+    void TwoDaysOldIsLate() {
+    	todoItem = new TodoItem("7", Instant.now().minus(1, ChronoUnit.DAYS), "todoItem créé il y a un jour");
+    	assertFalse(todoItem.isLate());
+    }
 }
